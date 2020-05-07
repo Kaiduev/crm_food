3b92049a98f8786c2c815f2a40d1bce33202a6eb
Kaiduev <you@example.com>
2020-05-07 16:37:39 +0600

    added tests

--- crm_food_app/tests/test_view.py
+++ crm_food_app/tests/test_view.py
@@ -15,9 +15,30 @@ class TableRequestsTestCase(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_table_post(self):
-        print("setup post")
-        client = APIClient()
+        print("Test POST table")
+        client = RequestsClient()
         url = 'http://testserver/tables'
-        data = {'name': 'Table№1'}
+        data = {"id": 1, "name": "Table№1"}
         response = client.post(url, data=data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # def test_table_put(self):
+    #     print("Setup EDIT Table")
+    #     self.table = Table.objects.create(name="Table№1")
+    #     url = '/tables/1'
+    #     response = self.client.get(url)
+    #     data = {"name": "table"}
+    #     factory = APIRequestFactory()
+    #     response_put = factory.post(url, data=data, format='json')
+    #     self.assertEqual(self.table.name, 'table')
+
+
+class RoleRequestsTestCase(APITestCase):
+
+    def test_role_get(self):
+        print("Test GET Role")
+        client = RequestsClient()
+        url = 'http://testserver/roles'
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
