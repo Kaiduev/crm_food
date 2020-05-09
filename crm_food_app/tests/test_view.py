6beeadecd744b7c6d9c33afe8880feb3e6c212fb
Kaiduev <you@example.com>
2020-05-09 16:17:22 +0600

    last tests

--- crm_food_app/tests/test_view.py
+++ crm_food_app/tests/test_view.py
@@ -49,3 +49,40 @@ class RoleRequestsTestCase(APITestCase):
         data = {"id": 1, "name": "waiter"}
         response = client.post(url, data=data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class DepartmentRequestsTestCase(APITestCase):
+
+    def test_department_get(self):
+        print("Test GET Department")
+        client = RequestsClient()
+        url = 'http://testserver/departments/'
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_department_post(self):
+        print("Test POST Department")
+        client = RequestsClient()
+        url = 'http://testserver/departments'
+        data = {"id": 1, "name": "kitchen" }
+        response = client.post(url, data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+class MealCategoryRequestsTestCase(APITestCase):
+
+    def test_category_get(self):
+        print("Test GET Request")
+        client = RequestsClient()
+        url = 'http://testserver/meal/categories/'
+        response = client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_category_post(self):
+        print("Test POST Request")
+        self.department = Department.objects.create(name="kitchen")
+        client = RequestsClient()
+        url = 'http://testserver/meal/categories/'
+        data = {"id": 1, "name": "Plov", "department": self.department}
+        response = client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
