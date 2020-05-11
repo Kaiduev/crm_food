f0c69f9070044fe3a2d1f0099e5fdd84df1a6798
Kaiduev <you@example.com>
2020-05-11 17:58:01 +0600

    Add Dockerfile and docker-compose.yml

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7*rsw6^l(()zw9if00zs2(9wb!!sn=es8l)@v&qtel2e7321(-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = True
-
 ALLOWED_HOSTS = ['*']
 
 
